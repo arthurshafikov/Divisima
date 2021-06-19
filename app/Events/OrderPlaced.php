c2763e49b0ac6aab7904e199de480eba0c2cf8a5
Wolf <wolf-front@yandex.ru>
2021-06-19 06:08:29 +0300

    refactor: events

--- app/Events/OrderPlaced.php
+++ app/Events/OrderPlaced.php
@@ -4,7 +4,6 @@ namespace App\Events;
 
 use App\Models\Order;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -13,23 +12,9 @@ class OrderPlaced
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $order;
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
+
     public function __construct(Order $order)
     {
         $this->order = $order;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
--- app/Events/ProductViewed.php
+++ app/Events/ProductViewed.php
@@ -3,33 +3,17 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
-use App\Models\Product;
 
 class ProductViewed
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $productId;
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
+
     public function __construct($id)
     {
         $this->productId = $id;
-    }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
     }
 }
