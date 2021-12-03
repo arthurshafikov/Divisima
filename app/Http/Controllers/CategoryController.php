28f9684a11a8b78377a4155e55dbcdaebfe55fdb
thewolf27 <wolf-front@yandex.ru>
2021-12-03 06:33:09 +0300

    refactor CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,18 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Filters\ProductFilter;
 use App\Models\Category;
-use App\Models\Product;
 use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
     public function category(ProductFilter $filters, $slug): View
     {
-        $category = Category::where('slug', $slug)->with('products')->firstOrFail();
+        $category = Category::whereSlug($slug)->with('products')->firstOrFail();
 
-        $products = Product::whereHas('category', function ($query) use ($slug) {
-            $query->where('slug', $slug);
-        })->filter($filters)->with('image')->paginate(self::$productsPerPage)->appends(request()->input());
+        $products = $category
+            ->products()
+            ->filter($filters)
+            ->with('image')
+            ->paginate(getOption('products_per_page'))
+            ->appends(request()->input());
 
         return view('shop')->with([
             'title'   =>  $category->name,
