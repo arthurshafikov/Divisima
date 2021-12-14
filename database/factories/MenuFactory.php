80824847c4c5a6e664c53674ce2a5c872529b820
thewolf27 <wolf-front@yandex.ru>
2021-12-14 06:36:00 +0300

    add MenuFactory and MenuItemFactory

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Menu;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MenuFactory extends Factory
+{
+    protected $model = Menu::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'location' => $this->faker->randomElement(['header', 'footer']),
+        ];
+    }
+}
---
+++ database/factories/MenuItemFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Menu;
+use App\Models\MenuItem;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MenuItemFactory extends Factory
+{
+    protected $model = MenuItem::class;
+
+    public function definition(): array
+    {
+        return [
+            'menu_id' => fn() => self::factoryForModel(Menu::class),
+            'name' => $this->faker->word,
+            'path' => $this->faker->domainWord,
+        ];
+    }
+}
